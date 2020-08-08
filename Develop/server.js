58f2afee4b9008218ac87efd6dc854d51c61e228
Ryan Simpkins <rythsimpkins@yahoo.com>
2020-08-08 15:30:30 -0700

    final

--- Develop/server.js
+++ Develop/server.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 
 const app = express();
 
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+var MONGODB_URI = "mongodb://localhost/budget";
 
 app.use(logger("dev"));
 
@@ -17,12 +17,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/mongoHeadlines", {
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-// mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
