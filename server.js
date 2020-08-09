de21ab51701aceb202004d5accfcd3dbff489a78
Ryan Simpkins <rythsimpkins@yahoo.com>
2020-08-08 17:42:12 -0700

    2

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://blakshep:Fieldy23!@ds047612.mlab.com:47612/heroku_3rkbqnls", {
+
+  useMongoClient: true,
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
